ca7756003c1d6bed3df473fe7b9fcd4923d29872
Alex Johnson <alexwj@gmx.com>
2016-05-05 15:25:20 -0400

    Documentation improved

--- PrivacyTool.java
+++ PrivacyTool.java
@@ -1,7 +1,3 @@
-/**
- * Privacy Tool
- */
-
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -32,26 +28,27 @@ public class PrivacyTool {
 
 class PrivacyPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	JFileChooser fileFinder = new JFileChooser();
-	File[] selectedFiles;
-	File currentFile;
-	String[] fileNames;
-	JLabel currentSelection;
-	JLabel imageArea;
-	int currentPos = 0;
+	JFileChooser fileFinder = new JFileChooser(); //File opener
+	File[] selectedFiles; //Array of files selected by the user
+	File currentFile; //File currently being previewed
+	String[] fileNames; //Array of strings of the names of the selected files
+	JLabel currentSelection; //Label that displays the name of the currently selected file
+	JLabel imageArea; //Label that displays the preview image of the currently selected file
+	int currentPos = 0; //Position in the selectedFiles array that currentFile will be taken from
 
 	//Panel that holds the preview image and buttons
 	public PrivacyPanel() {
-		setLayout(null);
+		setLayout(null); //Allows buttons to be places manually
 
-		JButton chooseFileButton = new JButton("Choose File(s)");
-		JButton leftButton = new JButton("Left");
-		JButton rightButton = new JButton("Right");
-		JButton singleDeleteButton = new JButton("Delete One");
-		JButton allDeleteButton = new JButton("Delete All");
+		JButton chooseFileButton = new JButton("Choose File(s)"); //Button to open file selection menu
+		JButton leftButton = new JButton("Left"); //Button to move left in the list of files
+		JButton rightButton = new JButton("Right"); //Button to move right in the list of files
+		JButton singleDeleteButton = new JButton("Delete One"); //Button to remove metadata from the current image
+		JButton allDeleteButton = new JButton("Delete All"); //Button to remove metadata from all images in the array selectedFiles
 		currentSelection = new JLabel("Please select a file / files.");
 		imageArea = new JLabel();
 
+		//Sets the locations and sizes of all buttons and labels
 		chooseFileButton.setBounds(8, 448, 128, 32);
 		leftButton.setBounds(8, 392, 128, 32);
 		rightButton.setBounds(256, 392, 128, 32);
@@ -60,15 +57,18 @@ class PrivacyPanel extends JPanel {
 		currentSelection.setBounds(136, 448, 256, 32);
 		imageArea.setBounds(16, 16, 360, 360);
 
+		//Adds action listeners to all buttons
 		chooseFileButton.addActionListener(new chooseFileListener());
 		leftButton.addActionListener(new leftListener());
 		rightButton.addActionListener(new rightListener());
 		singleDeleteButton.addActionListener(new singleDeleteListener());
 		allDeleteButton.addActionListener(new allDeleteListener());
 
+		//Sets the image preview area as a different color than the surrounding panel
 		imageArea.setOpaque(true);
 		imageArea.setBackground(Color.LIGHT_GRAY);
 
+		//Adds all buttons and labels to the panel
 		add(chooseFileButton);
 		add(currentSelection);
 		add(leftButton);
@@ -77,27 +77,26 @@ class PrivacyPanel extends JPanel {
 		add(allDeleteButton);
 		add(imageArea);
 
-		fileFinder.setMultiSelectionEnabled(true);
-		fileFinder.setFileFilter(new FileNameExtensionFilter("JPEG Images", "jpg"));
+		fileFinder.setMultiSelectionEnabled(true); //Allows the users to select multiple files
+		fileFinder.setFileFilter(new FileNameExtensionFilter("JPEG Images", "jpg")); //Allows the user to only select jpeg files
 	}
-	/*
-	 * Sets the current file to the file at the specified index in the file array
-	 * Updates the preview image
-	 * Updates the current selection text
-	 */
+	
+	//Sets the current file to the file at the specified index in the file array
+	//Updates the preview image
+	//Updates the current selection text
 	public void updatePreview() {
-		currentFile = selectedFiles[currentPos];
-		Image img = new ImageIcon(currentFile.toString()).getImage();
-		Image scaledImg = img.getScaledInstance(360, 360, Image.SCALE_FAST);
-		imageArea.setIcon(new ImageIcon(scaledImg));
-		currentSelection.setText(currentFile.getName());
+		currentFile = selectedFiles[currentPos]; //Sets the currentFile to the file from the selected location in the selectedFiles array
+		Image img = new ImageIcon(currentFile.toString()).getImage(); //Reads image from the selected file
+		Image scaledImg = img.getScaledInstance(360, 360, Image.SCALE_FAST); //Scales the image to fit the preview area
+		imageArea.setIcon(new ImageIcon(scaledImg)); //Displays the selected image in the preview area
+		currentSelection.setText(currentFile.getName()); //Displays the name of the currently selected file
 	}
 	//Removes exit data from file by rewriting over file with only image data
 	public void removeExif(File f) {
 		try {
-			BufferedImage b = ImageIO.read(f);
-			ImageIO.write(b, "jpg", f);
-		} catch (Exception e) {
+			BufferedImage b = ImageIO.read(f); //Reads in the file from the argument
+			ImageIO.write(b, "jpg", f); //Overwrites the selected file with only the image data, stripping the metadata
+		} catch (Exception e) { //Catches issues reading the file in
 			System.out.println("Privacy tool failed to clean the image!");
 		}
 	}
